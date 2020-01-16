e3e92c3507ec251f9997ea63b607034ca58dce0b
Rob Watson <rwatso@gmail.com>
2020-01-16 15:22:19 -0800

    Update reverseLineInputRevIter.cpp

--- reverseLineInputRevIter.cpp
+++ reverseLineInputRevIter.cpp
@@ -1,14 +1,20 @@
+
+// reverses line input in output by use of a reverse iterator. 
+
 #include <iostream>
 #include <string>
 
 using namespace std;
 
 int main() {
+    
     string input;
+    
     while(getline(cin, input)) {
         for(auto it = input.rbegin(); it != input.rend(); ++it) {
             cout << *it;
         }
         cout << '\n';
     }
+    
 }
